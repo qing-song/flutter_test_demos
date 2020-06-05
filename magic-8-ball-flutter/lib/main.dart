53fa7198b27893041c91121e42aacdbf7cae75bd
qingsong <qs_hero@163.com>
2020-06-05 08:11:23 +0800

    Magic 8 Ball app
    upload Magic 8 Ball app

---
+++ magic-8-ball-flutter/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(BallPage());
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          title: Text('Ask Me Anything'),
+        ),
+        body: Ball(),
+      )
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+
+  int ballNumber = 1;
+
+  void randomBallNumber() {
+    ballNumber = Random().nextInt(5) + 1;
+    print('i clicked $ballNumber');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+          onPressed: () {
+            setState(() {
+              randomBallNumber();
+            });
+          },
+          child: Image.asset('images/ball$ballNumber.png')),
+    );
+  }
+}
