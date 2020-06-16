f9b3aee47a4b14812c200d42516664abfe7078d0
qingsong <qs_hero@163.com>
2020-06-16 22:50:19 +0800

    add exercise demo
    add exercise demo

--- README.md
+++ README.md
@@ -104,3 +104,55 @@ class _BallState extends State<Ball> {
 }
 
 ```
+
+![xylophone-flutter](https://raw.githubusercontent.com/londonappbrewery/Images/master/xylophone-flutter.png)
+
+``` flutter
+
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playerSound(int soundNumber) {
+    final playCache = AudioCache();
+    playCache.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playerSound(soundNumber);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.blue, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.yellow, soundNumber: 5),
+              buildKey(color: Colors.purple, soundNumber: 6),
+              buildKey(color: Colors.teal, soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+```
---
+++ flutter_exercise_demo/lib/main.dart
@@ -0,0 +1,241 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(HomePageController());
+
+// Home Page
+class HomePageController extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Demo List",
+      theme: ThemeData(
+          primaryColor: Colors.teal
+      ),
+      home: HomePage(title: "Flutter Demo Home Page"),
+      routes: {
+        "XylophoneApp" : (BuildContext context) => XylophoneApp(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+
+  HomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("test"),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "XylophoneApp");
+            },
+            child: Text("XylophoneApp"),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push<String>(context, new MaterialPageRoute(builder: (BuildContext context){
+                  return MiCard();
+                }));
+              },
+              child: Text("MiCard"),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push<String>(context, new MaterialPageRoute(builder: (BuildContext context){
+                  return ContainerExercise();
+                }));
+              },
+              child: Text("XylophoneApp"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// ContainerExercise
+class ContainerExercise extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Text("Container"),
+        ),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+              ),
+
+            ],
+          ),
+        ),
+    );
+  }
+}
+
+
+
+// mi_card
+class MiCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Text("MiCard"),
+        ),
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/angela.jpg'),
+                ),
+                Text(
+                  'Angela Yu',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '+44 123 456 789',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    )),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'angela@email.com',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro'),
+                      ),
+                    ))
+              ],
+            )),
+    );
+  }
+}
+
+
+// XylophoneApp
+class XylophoneApp extends StatelessWidget {
+  void playerSound(int soundNumber) {
+    final playCache = AudioCache();
+    playCache.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playerSound(soundNumber);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("second page"),
+      ),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            buildKey(color: Colors.red, soundNumber: 1),
+            buildKey(color: Colors.orange, soundNumber: 2),
+            buildKey(color: Colors.blue, soundNumber: 3),
+            buildKey(color: Colors.green, soundNumber: 4),
+            buildKey(color: Colors.yellow, soundNumber: 5),
+            buildKey(color: Colors.purple, soundNumber: 6),
+            buildKey(color: Colors.teal, soundNumber: 7),
+          ],
+        ),
+      ),
+    );
+  }
+}
