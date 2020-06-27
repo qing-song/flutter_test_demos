628a0ab1a85d32b42abca67ac7f2dbc05848d862
qingsong <qs_hero@163.com>
2020-06-27 11:08:15 +0800

    finished destini exercise

--- flutter_exercise_demo/lib/main.dart
+++ flutter_exercise_demo/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutterexercisedemo/Destini/destini.dart';
 import 'package:flutterexercisedemo/Quizzler/quizzler.dart';
 import 'package:flutterexercisedemo/XylophoneApp/xylophone_app.dart';
+import 'package:flutterexercisedemo/mi_card/mi_card.dart';
 
 
 void main() => runApp(HomePageController());
@@ -37,6 +39,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("test"),
+        backgroundColor: Colors.teal,
       ),
       body: SafeArea(
         child: Column(
@@ -70,6 +73,14 @@ class _HomePageState extends State<HomePage> {
                 }));
               },
               child: Text("Quizzler"),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push<String>(context, new MaterialPageRoute(builder: (BuildContext context){
+                  return Destini();
+                }));
+              },
+              child: Text("Destini"),
             ),
           ],
         ),
@@ -124,85 +135,4 @@ class ContainerExercise extends StatelessWidget {
   }
 }
 
-
-
-// mi_card
-class MiCard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.teal,
-        appBar: AppBar(
-          title: Text("MiCard"),
-        ),
-        body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/angela.jpg'),
-                ),
-                Text(
-                  'Angela Yu',
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  'FLUTTER DEVELOPER',
-                  style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    color: Colors.teal.shade100,
-                    fontSize: 20.0,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                  width: 150.0,
-                  child: Divider(
-                    color: Colors.teal.shade100,
-                  ),
-                ),
-                Card(
-                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        '+44 123 456 789',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    )),
-                Card(
-                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.email,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        'angela@email.com',
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            color: Colors.teal.shade900,
-                            fontFamily: 'Source Sans Pro'),
-                      ),
-                    ))
-              ],
-            )),
-    );
-  }
-}
 
